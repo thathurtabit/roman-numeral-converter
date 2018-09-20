1eee66416045b6b8070227c8362777d2cfb95843
Stephen Fairbanks <biggste2000@hotmail.com>
2018-09-20 13:28:59 +0100

    Feat: Sup added to *

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -14,7 +14,7 @@ const Home = () => (
 		<h1>Number to <span>Roman Numeral</span> Converter</h1>
 		<p>(Gimme a number<sup>*</sup>, and I'll convert it to a Roman numeral.)</p>
 		<Converter />
-		<small>*<span>Max: 3999</span> - never mind why!</small>
+		<small><sup>*</sup><span>Max: 3999</span> - never mind why!</small>
 	</div>
 );
 
